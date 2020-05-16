b67d251411f6d285d87a6a72a40a9ae3e6632b43
Francisco Marcano <fjmn2001@gmail.com>
2020-05-16 12:39:13 -0400

    getting my userdata from google

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,39 +1,89 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzitripsapp/User/bloc/bloc_user.dart';
+import 'package:platzitripsapp/User/model/user.dart';
 import 'package:platzitripsapp/User/ui/widgets/button_bar.dart';
-import 'package:platzitripsapp/User/ui/widgets/user_info.dart';
+import 'package:platzitripsapp/User/ui/widgets/my_user_info.dart';
 
 class ProfileHeader extends StatelessWidget {
+
+  UserBloc _userBloc;
+  User user;
+
   @override
   Widget build(BuildContext context) {
 
-    final title = Text(
-      'Profile',
-      style: TextStyle(
-        fontFamily: 'Lato',
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-        fontSize: 30.0
-      ),
+    _userBloc = BlocProvider.of<UserBloc>(context);
+
+    return StreamBuilder(
+      stream: _userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch(snapshot.connectionState){
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return showProfileData(snapshot);
+          case ConnectionState.done:
+            return showProfileData(snapshot);
+        }
+      },
     );
 
-    return Container(
-      margin: EdgeInsets.only(
-        left: 20.0,
-        right: 20.0,
-        top: 50.0
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              title
-            ],
-          ),
-          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
-          ButtonsBar()
-        ],
-      ),
-    );
+  }
+
+  final title = Text(
+    'Profile',
+    style: TextStyle(
+      fontFamily: 'Lato',
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 30.0
+    ),
+  );
+
+  Widget showProfileData(AsyncSnapshot<FirebaseUser> snapshot) {
+    if(!snapshot.hasData || snapshot.hasError){
+      return Container(
+        margin: EdgeInsets.only(
+          left: 20.0,
+          right: 20.0,
+          top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text('without information.')
+          ],
+        ),
+      );
+    } else {
+      user = User(
+        name: snapshot.data.displayName,
+        email: snapshot.data.email,
+        photoURL: snapshot.data.photoUrl
+      );
+      return Container(
+        margin: EdgeInsets.only(
+          left: 20.0,
+          right: 20.0,
+          top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                title
+              ],
+            ),
+            MyUserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+    }
   }
 
 }
